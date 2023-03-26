41a3a946d43fbb06e675ba851c944c5b093f68c2
vini <vineethyadavnaluvala@gmail.com>
2023-03-26 23:53:12 +0530

    "files changed in test class"

--- src/main/java/in/vini/beans/Test.java
+++ src/main/java/in/vini/beans/Test.java
@@ -4,19 +4,12 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test 
-//stash command
 
-int i = 10;
-
-String msg = "hello"
-boolean flag = true;
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
 		UserService user = context.getBean(UserService.class);
 		user.printName(1);
-public void Demo{
-//under progress bug no 99
-}
+
 	}
 
 }
