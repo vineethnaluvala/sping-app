bbaac49771281689ca04150b2f4cce4a014de13d
vini <vineethyadavnaluvala@gmail.com>
2023-03-26 22:23:13 +0530

    java file modified

--- src/main/java/in/vini/beans/Test.java
+++ src/main/java/in/vini/beans/Test.java
@@ -4,7 +4,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test {
-
+int i = 10;
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
 		UserService user = context.getBean(UserService.class);
