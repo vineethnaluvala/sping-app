2c7af5b7a5778bf11c2434a8891fd85ee2498243
vini <vineethyadavnaluvala@gmail.com>
2023-03-26 23:33:55 +0530

    bug no 100

--- src/main/java/in/vini/beans/Test.java
+++ src/main/java/in/vini/beans/Test.java
@@ -9,10 +9,14 @@ public class Test
 int i = 10;
 
 String msg = "hello"
+boolean flag = true;
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
 		UserService user = context.getBean(UserService.class);
 		user.printName(1);
+public void Demo{
+//under progress bug no 99
+}
 	}
 
 }
