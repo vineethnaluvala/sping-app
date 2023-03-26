3817b8a55175bd7decb200c26e997943b02de97b
vineethnaluvala <128622786+vineethnaluvala@users.noreply.github.com>
2023-03-26 22:21:38 +0530

    Update Test.java

--- src/main/java/in/vini/beans/Test.java
+++ src/main/java/in/vini/beans/Test.java
@@ -4,7 +4,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test {
-
+String msg = "hello"
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
 		UserService user = context.getBean(UserService.class);
